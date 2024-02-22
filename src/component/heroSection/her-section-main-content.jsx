b99352402c1f4fe005f81e99b53b07f15ebbc691
Seemab Ahmed <seemab40615@gmail.com>
2024-02-22 22:36:12 +0500

    TextAnimation

--- src/component/heroSection/her-section-main-content.jsx
+++ src/component/heroSection/her-section-main-content.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import { TypingEffect } from './typingEffect';
+import {TypingEffect} from './typingEffect';
 export const HeroSectionMaincontent = () => {
   return (
     <div className="relative bg-cover bg-center h-full ">
       <div className="flex h-full items-end">
         <div className="flex justify-between w-full">
-          <div className="flex-col gap-y-[22px] max-w-[488px] w-full uppercase hero_section-gradient leading-7 hidden md:flex text-white font-hackathon text-2xl font-normal">   
-            <TypingEffect />
+          <div className="flex-col gap-y-[22px] max-w-[488px] w-full uppercase leading-7 hidden md:flex text-white font-hackathon text-2xl font-normal">   
+          <TypingEffect />
           </div>
-          <div className="flex flex-col gap-y-[22px] max-w-[488px] w-full uppercase hero_section-gradient leading-7 md:hidden">
+          <div className="flex flex-col gap-y-[22px] max-w-[488px] w-full uppercase leading-7 md:hidden">
             <p className="text-white font-hackathon text-3xl ">
               an
               <br /> n3v3r- <br /> end1ng <br /> exp3ri3n2e!
--- src/component/heroSection/typingEffect.jsx
+++ src/component/heroSection/typingEffect.jsx
@@ -1,20 +1,69 @@
+import { useState , useEffect } from 'react';
 import { TypeAnimation } from 'react-type-animation';
 
 export const TypingEffect = () => {
+  const [showDiv1, setShowDiv1] = useState(false);
+  const [showDiv2, setShowDiv2] = useState(false);
+  const [showDiv3, setShowDiv3] = useState(false);
+  const [hideDiv1, setHideDiv1] = useState(false);
+  const [hideDiv2, setHideDiv2] = useState(false);
+
+  useEffect(() => {
+    const timeout1 = setTimeout(() => setShowDiv1(true), 1000);
+    const timeout4 = setTimeout(()=>setHideDiv1(true),11000);
+    const timeout2 = setTimeout(() => setShowDiv2(true), 14000);
+    const timeout5 = setTimeout(()=>setHideDiv2(true),26000);
+    const timeout3 = setTimeout(() => setShowDiv3(true), 29000);
+
+    // Clear timeouts on component unmount to avoid memory leaks
+    return () => {
+      clearTimeout(timeout1);
+      clearTimeout(timeout2);
+      clearTimeout(timeout3);
+      clearTimeout(timeout4);
+      clearTimeout(timeout5);
+    };
+  }, []);
+
   return (
-    <TypeAnimation
-      sequence={[
-        // Same substring at the start will only be typed out once, initially
-        'Hello! This is Exelon 14. We need help immediately. Can anyone hear us?',
-        1000, // wait 1s before replacing "Mice" with "Hamsters"
-        'Hello! Is anybody out there? Hello? This is Tracer Ship Exelon 14. We have crashed. I repeat we have crashed...',
-        1000,
-        'What is this place? Is anyone around here??',
-        1000
-      ]}
-      wrapper="span"
-      speed={50}
-      repeat={0}
-    />
+    <div className="">
+      {showDiv1 && <div className={`${hideDiv1 ? 'fadeOutAnime' : ''}`}>
+      <TypeAnimation
+          sequence={[
+            'Hello! This is Exelon 14. We need help immediately. Can anyone hear us?',
+            3000,
+          ]}
+          wrapper="span"
+          speed={40}
+          repeat={0}
+          cursor={false}
+        />
+        </div>}
+      {showDiv2 && <div className={`${hideDiv2 ? 'fadeOutAnime' : ''}`}>
+      <TypeAnimation
+          sequence={[
+            'Hello! Is anybody out there? Hello? This is Tracer Ship Exelon 14. We have crashed. I repeat we have crashed...',
+            3000,
+          ]}
+          wrapper="span"
+          speed={40}
+          repeat={0}
+          cursor={false}
+        />
+         </div>}
+      {showDiv3 && <div className="">
+      <TypeAnimation
+        sequence={[
+          'What is this place? Is anyone around here??',
+          1000
+        ]}
+        wrapper="span"
+        speed={40}
+        repeat={0}
+        cursor={false}
+      />
+        </div>}
+    
+    </div>
   );
 };
